f9bf8df6baf1d8315f7576f0819624fc97821b71
Mikkel Back Koll <217236@RTS-365.dk>
2019-03-21 11:06:20 +0100

    parentElement_querySelector Opgave klaret

--- parentElement_querySelector/dist/js/index.js
+++ parentElement_querySelector/dist/js/index.js
@@ -1,31 +1,65 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
-	fetch("partials/nav_partial.html")
-	.then((promise)=>{
-		return promise.text();
-	})
-	.then((data)=>{
+	// fetch("partials/nav_partial.html")
+	// .then((promise)=>{
+	// 	return promise.text();
+	// })
+	// .then((data)=>{
 
-		let navTest = document.querySelector('.navTest');
+	// 	let navTest = document.querySelector('.navTest');
 
-		let articleTest = document.querySelector('.articleTest');
+	// 	let articleTest = document.querySelector('.articleTest');
 	
-		navTest.innerHTML = data;
+	// 	navTest.innerHTML = data;
 
 		
-		let liNavTestElements = navTest.querySelectorAll('.liTest');
+	// 	let liNavTestElements = navTest.querySelectorAll('.liTest');
 		
-		liNavTestElements.forEach((liTestElement)=>{
-			liTestElement.classList.add('color');
+	// 	liNavTestElements.forEach((liTestElement)=>{
+	// 		liTestElement.classList.add('color');
+	// 	})
+		
+	// 	articleTest.innerHTML = data;
+
+	// 	let liArticleTestElements = articleTest.querySelectorAll('.liTest');
+
+	// 	liArticleTestElements.forEach((liTestElement)=>{
+	// 		liTestElement.classList.add('color2');
+	// 	})
+
+
+
+	// })
+
+	let	itemElements = document.querySelectorAll('.item');
+
+
+	itemElements.forEach((itemElement)=>{
+
+				// itemElement.querySelector('.item-title').innerHTML += "X"
+				// itemElement.querySelector('.item-desc').innerHTML += "X"
+
+	let title =	itemElement.querySelector('.item-title');
+
+	let desc =	itemElement.querySelector('.item-desc');
+
+	title.innerHTML += "X";
+
+	desc.innerHTML += "X";
+
+		// itemElement.addEventListener('click',()=>{
+		// 	itemElement.querySelector('.item-title').innerHTML += "X";
+		// })
+
+		title.addEventListener('click',()=>{
+			title.innerHTML += "X";
 		})
-		
-		articleTest.innerHTML = data;
 
-		let liArticleTestElements = articleTest.querySelectorAll('.liTest');
-
-		liArticleTestElements.forEach((liTestElement)=>{
-			liTestElement.classList.add('color2');
+		desc.addEventListener('click',()=>{
+			desc.innerHTML += "X";
 		})
+
+
 
 	})
 
